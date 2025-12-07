1288aa9fb13927ffb848ff14342ad68f5f51c871
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-12-07 13:59:48 +0100

    made some changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,28 @@
 // src/App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <SearchBar />
-      <RecipeList />
-    </div>
+    <Router>
+      <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <SearchBar />
+        
+        <Routes>
+          {/* Home page showing list of recipes */}
+          <Route path="/" element={<RecipeList />} />
+          
+          {/* Dynamic route for recipe details */}
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
