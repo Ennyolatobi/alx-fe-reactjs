61d9a9c7dbea21d8682ff655e334a38380a312d6
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-12-13 15:46:36 +0100

    chore: add .env to gitignore for deployment

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -6,6 +6,9 @@ export default function App() {
     <div style={{ padding: 20, fontFamily: "system-ui, sans-serif" }}>
       <h1>GitHub User Search</h1>
       <Search />
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind is working
+      </h1>
     </div>
   );
 }
