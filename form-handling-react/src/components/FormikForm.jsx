0703a2f01378f9d486f59bfb6766c68c7f4f733c
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-12-14 20:01:54 +0100

    Refactor registration form to use Formik and Yup

---
+++ form-handling-react/src/components/FormikForm.jsx
@@ -0,0 +1,37 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+function FormikForm() {
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string().min(6, "Minimum 6 characters").required("Password is required"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        console.log("Formik Submit:", values);
+      }}
+    >
+      <Form>
+        <h2>Formik Registration</h2>
+
+        <Field name="username" placeholder="Username" />
+        <ErrorMessage name="username" component="div" />
+
+        <Field name="email" type="email" placeholder="Email" />
+        <ErrorMessage name="email" component="div" />
+
+        <Field name="password" type="password" placeholder="Password" />
+        <ErrorMessage name="password" component="div" />
+
+        <button type="submit">Register</button>
+      </Form>
+    </Formik>
+  );
+}
+
+export default FormikForm;
