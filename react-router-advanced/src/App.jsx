c14805164af313ae6a0acd9efe83c8a0eb4ab846
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-12-14 21:09:05 +0100

    advanced routing with react router

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Login from "./Login";
+import ProtectedRoute from "./ProtectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
