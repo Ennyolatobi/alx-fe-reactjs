5aefb43d4c69122c7a02f89588e55d5268dcbe68
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-12-14 20:50:05 +0100

    Implement data fetching with React Query

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchPosts = async () => {
+  const res = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return res.data;
+};
+
+function PostsComponent() {
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (error) return <p>Error loading posts</p>;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
+
+      <ul>
+        {data.slice(0, 10).map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
